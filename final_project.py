b3e9e3b6624a6164a0284e0f14d359a448958d74
Hadeel Ayman Daher <88348273+Hadeelay@users.noreply.github.com>
2023-06-25 21:27:15 +0300

    todo2

--- final_project.py
+++ final_project.py
@@ -7,10 +7,10 @@ Delivery Date :25/6/2023
 
 
 # TODO 2 Define Course Class and define constructor with
-# course_id (generated using uuid4) ,
-# course name (user_input) and
-# course mark (user_input)
-
+    def __init__(self, name, mark):
+        self.course_id = uuid.uuid4()
+        self.name = name
+        self.mark = mark
 
 class Student:
     # TODO 3 define static variable indicates total student count
